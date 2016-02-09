8d16e7294ea2f5f2304a21704a3c689fea2a64f9
mkfreeman <michael.k.freeman@gmail.com>
2016-02-09 10:12:36 -0800

    Cleaned exercises

--- exercise1.R
+++ exercise1.R
@@ -1,28 +1,2 @@
 # Get an example plotly map up and running
 
-# Copied this example from online here:
-# https://plot.ly/r/bubble-maps/
-
-library(plotly)
-df <- read.csv('https://raw.githubusercontent.com/plotly/datasets/master/2014_us_cities.csv')
-df$hover <- paste(df$name, "Population", df$pop/1e6, " million")
-
-df$q <- with(df, cut(pop, quantile(pop)))
-levels(df$q) <- paste(c("1st", "2nd", "3rd", "4th", "5th"), "Quantile")
-df$q <- as.ordered(df$q)
-
-g <- list(
-  scope = 'usa',
-  projection = list(type = 'albers usa'),
-  showland = TRUE,
-  landcolor = toRGB("gray85"),
-  subunitwidth = 1,
-  countrywidth = 1,
-  subunitcolor = toRGB("white"),
-  countrycolor = toRGB("white")
-)
-
-plot_ly(df, lon = lon, lat = lat, text = hover,
-        marker = list(size = sqrt(pop/10000) + 1),
-        color = q, type = 'scattergeo', locationmode = 'USA-states') %>%
-  layout(title = '2014 US city populations<br>(Click legend to toggle)', geo = g)
--- exercise3.R
+++ exercise3.R
@@ -14,29 +14,7 @@ data <- data %>%
         mutate(shots_fired = ifelse(is.na(shots_fired), mean(shots_fired, na.rm = T), shots_fired))
 
 # Create a bubble map of the data
-g <- list(
-  scope = 'usa', 
-  projection = list(type = 'albers usa'),
-  showland = FALSE,
-  landcolor = toRGB("gray85"), 
-  subunitwidth = 1,
-  countrywidth = 0,
-  subunitcolor = toRGB("gray85"), 
-  countrycolor = toRGB('gray85')
-)
 
 ### Bonus: create informative hover text ###
-
-# Create graph
-plot_ly(data, 
-        lon = lng, 
-        lat = lat, 
-        text = paste('<b>Name:</b>', data[,"Victim Name"], '<br><b>Shots Fired:</b>', data$shots_fired),
-        hoverinfo = 'text',
-        marker = list(size = 1 + shots_fired/3, opacity = .4, color = 'red'),
-        type = 'scattergeo', 
-        locationmode = 'USA-states'
-) %>%
-  layout(title = 'Crowdsourced Police Shootings', geo = g)
 
 ### Bonus: Use multiple colors ###
