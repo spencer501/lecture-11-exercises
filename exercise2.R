f07c54fb60f73fe924a7cd9a616d12c2d292a1b9
Spencer <spencer.p.816@gmail.com>
2016-02-10 16:15:28 -0800

    Start 'exercise2.R'

--- exercise2.R
+++ exercise2.R
@@ -1,12 +1,13 @@
 # Figure out what each line of code actually does!
 
 library(plotly)
+library(dplyr)
 
 # I suggest you fiddle with the mapping options,
 # Then come back and look at this data wrangling, if you have the time
 
 df <- read.csv('https://raw.githubusercontent.com/plotly/datasets/master/2014_us_cities.csv')
-df$hover <- paste(df$name, "Population", df$pop/1e6, " million") # what does this do?
+df$hover <- paste(df$name, "Population", df$pop/1e6, " million") # what does this do? A: Creates readable hover text from data
 df$q <- with(df, cut(pop, quantile(pop))) # what does this do????
 levels(df$q) <- paste(c("1st", "2nd", "3rd", "4th", "5th"), "Quantile") # what does this do?
 df$q <- as.ordered(df$q) # what does this do?
@@ -33,3 +34,5 @@ plot_ly(df,
         ) %>%
   # what if you don't pass this into the layout function?
 layout(title = '2014 US city populations<br>(Click legend to toggle)', geo = g)
+
+
